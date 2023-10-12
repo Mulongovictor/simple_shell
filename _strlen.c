2a17c60f5f314f90d65d7728b6284c5243b87b87
Mulongovictor <mulongovibu@gmail.com>
2023-10-12 04:06:54 -0700

    _strlen function added

---
+++ _strlen.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+/**
+ * _strlen - counts the number of characters in a string
+ * @str: is the pointer to the string whose length is counted
+ *
+ * Return: the size of the string count.
+ */
+
+int _strlen(char *str)
+{
+	int i, countchar = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		countchar++;
+	}
+	return (countchar);
+}
